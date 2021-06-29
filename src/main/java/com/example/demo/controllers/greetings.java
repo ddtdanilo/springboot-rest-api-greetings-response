4abcac44f0c4b751fca2dd46659c112142fe7cff
Danilo <danilodt@gmail.com>
2021-06-29 02:09:48 -0500

    Create a response for GET, PUT, POST DELETE

--- src/main/java/com/example/demo/controllers/greetings.java
+++ src/main/java/com/example/demo/controllers/greetings.java
@@ -1,6 +1,9 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,33 @@ public class greetings {
     public String getResponse(@RequestParam String name) {
 
         String output = "Hello, " + name + " from devco. GET method";
+
+        return output;
+
+    }
+
+    @PostMapping("/greetings")
+    public String postResponse(@RequestParam String name) {
+
+        String output = "Hello, " + name + " from devco. PUT method";
+
+        return output;
+
+    }
+
+    @DeleteMapping("/greetings")
+    public String deleteResponse(@RequestParam String name) {
+
+        String output = "Hello, " + name + " from devco. DELETE method";
+
+        return output;
+
+    }
+    
+    @PutMapping("/greetings")
+    public String putResponse(@RequestParam String name) {
+
+        String output = "Hello, " + name + " from devco. PUT method";
 
         return output;
 
