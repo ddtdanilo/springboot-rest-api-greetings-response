d314f431f1dd98843b04e5379e695e6ca9091ebd
Danilo <danilodt@gmail.com>
2021-06-30 18:53:16 -0500

    github actions: Run the deploy code in one line

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/repos/springboot-rest-api-greetings-response
-
-pkill screen # Kill the service
-
-git stash # For not taking the files generated if the case
-git checkout main
-git pull
-
-chmod +x gradlew
-sudo ./gradlew build
-screen -d -m bash -c "sudo ./gradlew bootRun" # Create a screen sesion
-
-exit
-
+cd /home/ubuntu/repos/springboot-rest-api-greetings-response;pkill screen;git stash;git checkout main;git pull;chmod +x gradlew;sudo ./gradlew build;screen -d -m bash -c "sudo ./gradlew bootRun";exit | sudo ssh -tt -o "StrictHostKeyChecking no" -i key.pem ubuntu@3.130.50.98 /bin/bash
--- run_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/repos/springboot-rest-api-greetings-response
-screen -d -m bash -c "sudo ./gradlew bootRun" # Create a screen sesion
-sleep 5
-
-
-exit
-
