b8488d79761981c360fffc9a914284d74d81a0da
Danilo <danilodt@gmail.com>
2021-06-30 15:14:09 -0500

    github actions: fix some errors in deployment

--- deploy_server.sh
+++ deploy_server.sh
@@ -4,11 +4,12 @@ cd /home/ubuntu/repos/springboot-rest-api-greetings-response
 pkill screen # Kill the service
 
 git stash # For not taking the files generated if the case
-git checkout origin/main
+git checkout main
 git pull
 
-./gradlew build
-screen -d -m bash -c "./gradlew bootRun" # Create a screen sesion
+chmod +x gradlew
+sudo ./gradlew build
+screen -d -m bash -c "sudo ./gradlew bootRun" # Create a screen sesion
 
 exit
 
